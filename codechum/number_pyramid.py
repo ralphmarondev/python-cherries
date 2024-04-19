42b1acc783037f74e59b3199765047ad470d0686
Ralph Maron Eda <edaralphmaron@gmail.com>
2024-04-19 10:03:32 +0800

    implemented number-pyramid pattern

--- codechum/number_pyramid.py
+++ codechum/number_pyramid.py
@@ -1,4 +1,11 @@
-n = int(input('Enter the number of rows: '))
+rows = int(input('Enter the number of rows: '))
 
-for i in range(n):
-    pass
+for i in range(1, rows + 1):
+    # printing spaces
+    print(' ' * (rows - i), end='')
+    # printing numbers
+    for j in range(1, i):
+        print(j, end='')
+    for j in range(i, 0, -1):
+        print(j, end='')
+    print()
